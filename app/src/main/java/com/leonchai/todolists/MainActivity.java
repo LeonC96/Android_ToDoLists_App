cbf8aece33f22f2b493abff4d75e1773a7b5d6e1
Leon Chai <leonchai964@gmail.com>
2018-05-12 02:44:52 -0700

    added tabs

--- app/src/main/java/com/leonchai/todolists/MainActivity.java
+++ app/src/main/java/com/leonchai/todolists/MainActivity.java
@@ -1,13 +1,52 @@
 package com.leonchai.todolists;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        final FirebaseUser user = auth.getCurrentUser();
+
+        if(user != null && (user.getDisplayName() != null || !user.getDisplayName().isEmpty())){
+            String name = getIntent().getStringExtra("name");
+            UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(name)
+                    .build();
+            user.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        System.out.println("User profile updated");
+                        System.out.println(user.getDisplayName());
+                    }
+                }
+            });
+        } else {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+
+        System.out.println(user.getDisplayName());
     }
 }
