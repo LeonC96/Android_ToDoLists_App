27dbf661a2a5567ba60f1e9d493489d6a1acb7d4
Leon Chai <leonchai964@gmail.com>
2018-06-11 02:13:58 -0700

    update every fragments

--- app/src/main/java/com/leonchai/todolists/DoingFragment.java
+++ app/src/main/java/com/leonchai/todolists/DoingFragment.java
@@ -5,9 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DoingFragment extends Fragment {
+
+    private ListView doingList;
+    private TaskAdapter adapter;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     public DoingFragment() {
         // Required empty public constructor
@@ -19,13 +32,59 @@ public class DoingFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doing, container, false);
+        View view = inflater.inflate(R.layout.fragment_do, container, false);
+
+        System.out.println("onCreateView");
+
+        doingList = (ListView) view.findViewById(R.id.doListView);
+
+        ArrayList<TaskModel> tasks = new ArrayList<>();
+        tasks.add(new TaskModel("Do this", "01/20/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do that", "06/20/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/20/29", user.getDisplayName()));
+        tasks.add(new TaskModel("Do and this", "01/24/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/20/18", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/06/19", user.getDisplayName()));
+
+        adapter = new TaskAdapter(getActivity(),tasks);
+        doingList.setAdapter(adapter);
+
+        //STILL NEEDS TO WAIT .... maybe dont matter
+        FirebaseDB.getList(user.getUid(), "doTasks", new FirebaseDB.FirebaseCallback() {
+            @Override
+            public void onCallback(List<TaskModel> tasks) {
+                System.out.println(tasks.get(0).getName());
+                System.out.println("DONE");
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        System.out.println("RESUME");
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(isVisibleToUser){
+            System.out.println("VISIBLE");
+            if(getView() != null){
+                System.out.println("THERES A VIEW");
+            }
+        } else {
+            System.out.println("NOT VISIBLE");
+        }
     }
 
 }
--- app/src/main/java/com/leonchai/todolists/DoneFragment.java
+++ app/src/main/java/com/leonchai/todolists/DoneFragment.java
@@ -5,8 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DoneFragment extends Fragment {
+
+    private ListView doneList;
+    private TaskAdapter adapter;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     public DoneFragment() {
         // Required empty public constructor
@@ -16,13 +29,59 @@ public class DoneFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_done, container, false);
+        View view = inflater.inflate(R.layout.fragment_do, container, false);
+
+        System.out.println("onCreateView");
+
+        doneList = (ListView) view.findViewById(R.id.doListView);
+
+        ArrayList<TaskModel> tasks = new ArrayList<>();
+        tasks.add(new TaskModel("Do this", "01/20/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do that", "06/20/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/20/29", user.getDisplayName()));
+        tasks.add(new TaskModel("Do and this", "01/24/19", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/20/18", user.getDisplayName()));
+        tasks.add(new TaskModel("Do this", "01/06/19", user.getDisplayName()));
+
+        adapter = new TaskAdapter(getActivity(),tasks);
+        doneList.setAdapter(adapter);
+
+        //STILL NEEDS TO WAIT .... maybe dont matter
+        FirebaseDB.getList(user.getUid(), "doTasks", new FirebaseDB.FirebaseCallback() {
+            @Override
+            public void onCallback(List<TaskModel> tasks) {
+                System.out.println(tasks.get(0).getName());
+                System.out.println("DONE");
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        System.out.println("RESUME");
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(isVisibleToUser){
+            System.out.println("VISIBLE");
+            if(getView() != null){
+                System.out.println("THERES A VIEW");
+            }
+        } else {
+            System.out.println("NOT VISIBLE");
+        }
     }
 
 }
